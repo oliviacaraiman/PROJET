f622c46a5b595205a14937dda555d6f0c99ea048
oliviacaraiman <caraimanolivia@gmail.com>
2016-05-21 23:28:28 +0200

    Update Arme.java

--- Arme.java
+++ Arme.java
@@ -1,28 +1,5 @@
 public class Arme {
 	
-	
-	public static void main (String args[]) {
-		
-		// Creation des armes
-		Arme a1 = new Arme(1, "Pierre", 5);
-		Arme a2 = new Arme(2, "Bouteille", 10);
-		Arme a3 = new Arme(3, "Baton", 15);
-		Arme a4 = new Arme(4, "Couteau", 20);
-		Arme a5 = new Arme(5, "Revolver", 40);
-		Arme a6 = new Arme(6, "Fusile", 50);
-		Arme a7 = new Arme(7, "Grenade", 70);
-		
-		// Presentation des armes
-		System.out.println(a1);
-		System.out.println(a2);
-		System.out.println(a3);
-		System.out.println(a4);
-		System.out.println(a5);
-		System.out.println(a6);
-		System.out.println(a7);
-		
-	}
-	
 	// Attributs
     private String nom;
     private int coutAttaque;
@@ -31,15 +8,42 @@ public class Arme {
     // Constructeurs
     public Arme(int numero){
         this.numero = numero;
+        int n = this.numero;
+        // Noms
+        if(n == 1) {
+        	this.nom = "Pierre";
+        	this.coutAttaque = 5;
+        }
+        else if(n == 2) {
+        	this.nom = "Bouteille";
+        	this.coutAttaque = 10;
+        }
+        else if(n == 3) {
+        	this.nom = "Baton";
+        	this.coutAttaque = 15;
+        }
+        else if(n == 4) {
+        	this.nom = "Couteau";
+        	this.coutAttaque = 20;
+        }
+        else if(n == 5) {
+        	this.nom = "Revolver";
+        	this.coutAttaque = 40;
+        }
+        else if(n == 6) {
+        	this.nom = "Fusile";
+        	this.coutAttaque = 50;
+        }
+        else if (n == 7) {
+        	this.nom = "Grenade";
+        	this.coutAttaque = 70;
+        }
     }
+    //fin constructeur
     
-    public Arme(int numero, String nom, int coutAttaque) {
-		this.numero = numero;
-		this.nom = nom;
-		this.coutAttaque = coutAttaque;
-	}
     
-    // Methodes
+    
+    //Methodes
     public int getNumero(){
         return numero;
     }
