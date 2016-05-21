08750e1c7f53cc2c3cd3f42ddadab1ab72a1cc7d
oliviacaraiman <caraimanolivia@gmail.com>
2016-05-21 23:31:16 +0200

    Update Cowboy.java

--- Cowboy.java
+++ Cowboy.java
@@ -1,71 +1,73 @@
-public class Arme {
-	
-	// Attributs
+public class Cowboy {
+
+    // Attributs
     private String nom;
-    private int coutAttaque;
-    private int numero;
+    private int a1, a2, a3;
+    private Arme arme1, arme2, arme3;
+    private int vie;
+    private int energie; 
     
-    // Constructeurs
-    public Arme(int numero){
-        this.numero = numero;
-        int n = this.numero;
-        // Noms
-        if(n == 1) {
-        	this.nom = "Pierre";
-        	this.coutAttaque = 5;
-        }
-        else if(n == 2) {
-        	this.nom = "Bouteille";
-        	this.coutAttaque = 10;
-        }
-        else if(n == 3) {
-        	this.nom = "Baton";
-        	this.coutAttaque = 15;
-        }
-        else if(n == 4) {
-        	this.nom = "Couteau";
-        	this.coutAttaque = 20;
-        }
-        else if(n == 5) {
-        	this.nom = "Revolver";
-        	this.coutAttaque = 40;
-        }
-        else if(n == 6) {
-        	this.nom = "Fusile";
-        	this.coutAttaque = 50;
-        }
-        else if (n == 7) {
-        	this.nom = "Grenade";
-        	this.coutAttaque = 70;
-        }
+    // Constructeur
+    public Cowboy(String nom, int a1, int a2, int a3) {    				// Constructeur tres long
+        this.nom = nom;
+        this.vie = 100;
+        this.energie = 100;
+        this.a1= a1;
+        this.a2= a2;
+        this.a3= a3;
+        this.arme1 = new Arme(a1);
+        this.arme2 = new Arme(a2);
+        this.arme3 = new Arme(a3);
     }
-    //fin constructeur
-    
-    //Methodes
-    public int getNumero(){
-        return numero;
-    }
-    
-    public int getCoutAttaque(){
-        return coutAttaque;
-    }
-	
-	public String getNomArme() {
-		return nom;
+        
+	// Accesseurs
+	public String getNomJoueur() {
+		return this.nom;
 	}
 	
-    public String toString(){
-        String res = this.numero + ") " + this.nom + " : Capacite de frappe = " + this.coutAttaque;
+	public int getVie() {
+		return this.vie;
+	}
+	
+	public int getEnergie() {
+		return this.energie;
+	}
+	
+	public int getNbArme1() {
+		return this.a1;
+	}
+	
+	public int getNbArme2() {
+		return this.a2;
+	}
+	
+	public int getNbArme3() {
+		return this.a3;
+	}
+	
+	public Arme getArme1() {
+		return this.arme1;
+	}
+	
+	public Arme getArme2() {
+		return this.arme2;
+	}
+	
+	public Arme getArme3() {
+		return this.arme3;
+	}
+			
+	public String toString() {
+        String res = nom + "(" + this.getArme1().getNomArme() + ", "
+					+ this.getArme2().getNomArme() + ", "+this.getArme3().getNomArme()+")";
+        
         return res;            
     }
-    
-    
+			  
 }
-
-
+            
         
-        
-        
+              
         
         
         
